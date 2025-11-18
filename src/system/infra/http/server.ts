e10701f791149fade10d370cfa2edab4fc2ad8ac
Nicola Luongo <=>
2025-11-18 13:51:53 +0100

    save

--- src/system/infra/http/server.ts
+++ src/system/infra/http/server.ts
@@ -1,28 +1,15 @@
 import semver from 'semver';
-import 'reflect-metadata';
 
-// Tsconfig paths
-// eslint-disable-next-line import/no-extraneous-dependencies
-
-// Environment import
-import './environment';
-
-// Service import
-import { Bootstrap } from './bootstrap';
-
-// Server import
 import { App } from './app';
 
-// Start server
 const app = new App().server;
 
-// Start listen
-Bootstrap.then(() => app.listen(process.env.APP_PORT, () => {
+app.listen(process.env.PORT, () => {
   console.log(
-    `using Node.js ${semver.clean(process.version)} running at port ${
-      process.env.APP_PORT
+    `using Node.js ${semver.clean(process.version)} running on port ${
+      process.env.PORT
     }.`,
   );
-}));
+});
 
 export { app };
